96645f2b4f9bc970d9282fc7d20b9d8224b04457
momo_toms <gutomojr@gmail.com>
2019-12-04 20:40:25 +0700

    commit

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import {ProductConsumer} from '../Context';
+import {Link} from 'react-router-dom';
+import {ButtonContainer} from './Button';
 
 class detail extends Component {
     render() {
         return (
-            <div>
-               <h3>Ini Halaman Detail</h3> 
-            </div>
+            <ProductConsumer>
+                {value =>{
+                   const {id, company, img, info, price, title, inCart} = value.detailProduct;
+                 return (
+                     <div className="container py-5">
+                        <div className="row">
+                            <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                <h1>
+                                  Model : {title}
+                                </h1>
+
+                            </div>
+                        </div>
+                     </div>
+                 )  
+                }}
+            </ProductConsumer>
         );
     }
 }
